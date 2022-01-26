9eb3bdb7e6edd5d3060689da67d31bd2120905ff
cjpen <cjpen@me.com>
2022-01-26 00:06:09 -0800

    Fix New Coven Feature

--- controllers/covens.js
+++ controllers/covens.js
@@ -1,9 +1,14 @@
 const Coven = require('../models/coven');
 
 module.exports = {
+    index,
     new: newCoven
 };
 
 function newCoven(req, res) {
     res.render('covens/new', { title: "Found A Coven"});
+}
+
+function index(req, res) {
+    res.render('covens', { title: "My Covens" })
 }
--- routes/covens.js
+++ routes/covens.js
@@ -5,9 +5,9 @@ var isLoggedIn = require('../config/auth');
 
 // Get /covens (display my covens)
 // router.get
-
+router.get("/", isLoggedIn, covensCtrl.index);
 // GET /covens/new (create a new coven)
-router.get("/new", isLoggedIn, covensCtrl.new)
+router.get("/new", isLoggedIn, covensCtrl.new);
 // GET /covens/:id (show coven details, including description and upcoming viewings)
 
 // EDIT /covens/:id/edit (edit details of the Coven)
