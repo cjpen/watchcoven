54dadb22678977fd1b43845b56ff66674f5e494f
cjpen <cjpen@me.com>
2022-01-26 09:13:05 -0800

    Update new post router

--- controllers/covens.js
+++ controllers/covens.js
@@ -2,7 +2,16 @@ const Coven = require('../models/coven');
 
 module.exports = {
     index,
-    new: newCoven
+    new: newCoven,
+    create
+};
+
+function create(req, res) {
+    var coven = new Coven(req.body);
+    coven.save(function (err) {
+        if (err) return res.redirect('/coven/new');
+        res.redirect('/coven/:id')
+    });
 };
 
 function newCoven(req, res) {
--- routes/covens.js
+++ routes/covens.js
@@ -8,6 +8,8 @@ var isLoggedIn = require('../config/auth');
 router.get("/", isLoggedIn, covensCtrl.index);
 // GET /covens/new (create a new coven)
 router.get("/new", isLoggedIn, covensCtrl.new);
+// POST /covens (Handle a new coven being submitted)
+router.post("/", isLoggedIn, covensCtrl.create);
 // GET /covens/:id (show coven details, including description and upcoming viewings)
 
 // EDIT /covens/:id/edit (edit details of the Coven)
